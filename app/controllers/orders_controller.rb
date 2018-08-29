d838c43e761bec686d8065a9476c7368b9b3975a
you@example.com <bertynboulikou@gmail.com>
2018-08-29 17:20:10 +0200

    modif order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,5 +2,27 @@ class OrdersController < ApplicationController
     def create
        @item=Item.find(params:item_id)
         @order=Order.create!(card_item_id)
+
+        customer = Stripe::Customer.create(
+            email: params['email'],
+            source:'src_18eYalAHEMiOZZp1l9ZTjSU0',
+          )
+
+        charge= Stripe::Charge.create(
+            customer: customer.id,
+            amount: @order.price,
+            description: "Payement photo de #{}",
+            currency: 'eur'
+        )
+
+        @order.update(payement: charge.to_json,state: 'paid')
+        redirect_to order_path(@order)
+        
+        rescue Stripe::CardError => e
+            flash[:alert]=e.message
+    end
+
+    def show
+        @command= current_user.orders.where(state: 'paid')
     end
 end
