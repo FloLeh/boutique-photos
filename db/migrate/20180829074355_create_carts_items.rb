d1d3e067fdb618a6742596dc2b35913e08db2962
FloLeh <florianlehuede@gmail.com>
2018-08-29 14:55:59 +0200

    order

--- db/migrate/20180829074355_create_carts_items.rb
+++ db/migrate/20180829074355_create_carts_items.rb
@@ -3,6 +3,7 @@ class CreateCartsItems < ActiveRecord::Migration[5.2]
     create_table :carts_items, id: false do |t|
       t.belongs_to :cart, foreign_key: true
       t.belongs_to :item, foreign_key: true
+
     end
   end
 end
