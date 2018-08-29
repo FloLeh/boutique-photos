442a9c562cff9015f208c3aac32ead876dad47a7
FloLeh <florianlehuede@gmail.com>
2018-08-29 16:06:19 +0200

    zefrgtghj

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_29_092537) do
+ActiveRecord::Schema.define(version: 2018_08_29_074355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,15 +38,6 @@ ActiveRecord::Schema.define(version: 2018_08_29_092537) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "orders", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "cart_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["cart_id"], name: "index_orders_on_cart_id"
-    t.index ["user_id"], name: "index_orders_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -69,6 +60,4 @@ ActiveRecord::Schema.define(version: 2018_08_29_092537) do
   add_foreign_key "carts", "users"
   add_foreign_key "carts_items", "carts"
   add_foreign_key "carts_items", "items"
-  add_foreign_key "orders", "carts"
-  add_foreign_key "orders", "users"
 end
